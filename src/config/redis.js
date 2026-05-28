8cdf8b1c12166601ee66ddd81193d08f9747554a
Phyo Kyaw San <phyojupiter9@gmail.com>
2026-05-28 14:48:59 +0630

    temp test

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,4 +1,4 @@
-import Redis from "ioredis";
+/* import Redis from "ioredis";
 
 const redis = new Redis({
   host: process.env.REDIS_HOST,
@@ -18,6 +18,19 @@ const redis = new Redis({
   reconnectOnError() {
     return true;
   },
+});
+
+redis.on("connect", () => console.log("✅ Redis connected"));
+redis.on("error", (err) => console.error("❌ Redis error:", err));
+
+export default redis;
+*/
+
+import Redis from "ioredis";
+
+const redis = new Redis({
+  host: process.env.REDIS_HOST || "127.0.0.1",
+  port: process.env.REDIS_PORT || 6379,
 });
 
 redis.on("connect", () => console.log("✅ Redis connected"));
